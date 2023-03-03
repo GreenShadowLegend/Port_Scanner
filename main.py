f91e6d21e3579907ea6a246bc6a479759a5d2079
Charon's Ghost <98899162+GreenShadowLegend@users.noreply.github.com>
2023-03-03 21:35:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+ascii_banner = pyfiglet.figlet_format("PORT   SCANNER made   by CharonSec")
+print(ascii_banner)
+target = input(str("Ip: "))
+print("_"*50)
+print("Scanning "+target)
+print("Scanning started at "+str(datetime.now()))
+print("_"*50)
+try:
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print("[+] Port {} is open.".format(port))
+            s.close()
+except KeyboardInterrupt:
+    print("\n Exiting")
+    sys.exit()
+except socket.error:
+    print('\ Host is not responding')
+    sys.exit()
